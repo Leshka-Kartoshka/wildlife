1dd453aa4f5d5cd6024fd1d5caa70948e8a598b9
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-03-02 12:15:04 +0300

    feat: webpage complite

--- scripts.js
+++ scripts.js
@@ -17,15 +17,32 @@ var changeSize = function(){
 let timer = setInterval(changeSize, 1000);
 
 var slide = function(dir){
-    step++;
+    
     if(dir == "right"){
+        step++;
         offset += elementWidth;
     }
     else if(dir == "left"){
+        step--;
         offset -= elementWidth;
     }
     else{
         offset = 0;
+    }
+    if(document.getElementsByClassName("container")[0].offsetWidth >= 767 && step == 2){
+        
+    }
+    else if(document.getElementsByClassName("container")[0].offsetWidth >= 767 && step == -1){
+        step = 1;
+        offset = elementWidth;
+    }
+    if(document.getElementsByClassName("container")[0].offsetWidth <= 766 && step == 3){
+        step = 0;
+        offset = 0;
+    }
+    else if(document.getElementsByClassName("container")[0].offsetWidth <= 766 && step == -1){
+        step = 2;
+        offset = 2 * elementWidth;
     }
     slider.style.left = -offset + 'px';
 }
